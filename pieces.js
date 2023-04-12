b888f02a1468323c100480b80eea54086cfe687c
david <cognet.david@hotmail.fr>
2023-04-12 09:53:31 +0200

    ajout de la boucle for

--- pieces.js
+++ pieces.js
@@ -2,36 +2,42 @@
 const response = await fetch("pieces-autos.json");
 const pieces = await response.json();
 
-// création des balises avec createElement
-const article = pieces[0];
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix : ${article.prix} € (${
-  article.prix < 35 ? "€" : "€€€"
-})`;
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+for (let i = 0; i < pieces.length; i++) {
+  const article = pieces[i];
+  // Récupération de l'élément du DOM qui accueillera les fiches
 
-//Création des élements pour l'exercice
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText =
-  article.description ?? "(Pas de description pour le moment.)";
+  const sectionFiches = document.querySelector(".fiches");
+  // Création d’une balise dédiée à une pièce automobile
+  const pieceElement = document.createElement("article");
 
-const disponibiliteElement = document.createElement("p");
-disponibiliteElement.innerText = article.disponibilite
-  ? "En stock"
-  : "Rupture de stock";
+  // création des balises avec createElement
+  const imageElement = document.createElement("img");
+  imageElement.src = article.image;
+  const nomElement = document.createElement("h2");
+  nomElement.innerText = article.nom;
+  const prixElement = document.createElement("p");
+  prixElement.innerText = `Prix : ${article.prix} € (${
+    article.prix < 35 ? "€" : "€€€"
+  })`;
+  const categorieElement = document.createElement("p");
+  categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+  const descriptionElement = document.createElement("p");
+  descriptionElement.innerText =
+    article.description ?? "(Pas de description pour le moment.)";
+  const disponibiliteElement = document.createElement("p");
+  disponibiliteElement.innerText = article.disponibilite
+    ? "En stock"
+    : "Rupture de stock";
 
-// Rattacher les éléments au reste du document
-// rattachement des balises au DOM
-const sectionFiches = document.querySelector(".fiches");
+  // Rattacher les éléments au reste du document
+  // rattachement des balises au DOM
 
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(disponibiliteElement);
+  sectionFiches.appendChild(pieceElement);
+
+  pieceElement.appendChild(imageElement);
+  pieceElement.appendChild(nomElement);
+  pieceElement.appendChild(prixElement);
+  pieceElement.appendChild(categorieElement);
+  pieceElement.appendChild(descriptionElement);
+  pieceElement.appendChild(disponibiliteElement);
+}
