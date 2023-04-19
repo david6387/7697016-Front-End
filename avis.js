8ed01bb1b6dad67a692e1e7bdacdfe2c56970c3d
david <cognet.david@hotmail.fr>
2023-04-19 22:45:28 +0200

    ajout d'un bouton sur chaque fiche produit

---
+++ avis.js
@@ -0,0 +1,9 @@
+export function ajoutListenerAvis() {
+    const piecesElements = document.querySelectorAll(".fiches article button");
+
+    for (let i = 0 < piecesElements.length; i++) {
+        piecesElements[i].addEventListener("click", async function (event) {
+            
+        })
+    }
+}
